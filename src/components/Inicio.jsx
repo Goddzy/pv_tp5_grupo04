af0e4ed2ae19791940bf8ad025e9bfc7ad90e6fc
Lucaszs-1 <lucasorrabaliz24@gmail.com>
2025-05-30 21:46:07 -0300

    Orrabaliz Lucas - Agregue un carrusel al main

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,3 +1,5 @@
+import Carousel from 'react-bootstrap/Carousel';
+
 function Inicio() {
   return (
     <div className="home-container d-flex align-items-center justify-content-center text-center">
@@ -6,6 +8,28 @@ function Inicio() {
         <p className="lead text-secondary my-5">
           Esta plataforma te permite llevar un control completo de tus estudiantes: altas, ediciones, consultas y mucho más.
         </p>
+        <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="../public/images/fac.jpg"
+        style={{ height: '600px', objectFit: 'cover' }}
+    />
+    <Carousel.Caption>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="../public/images/facu.webp"
+        style={{ height: '600px', objectFit: 'cover' }}
+    />
+    <Carousel.Caption>
+    </Carousel.Caption>
+  </Carousel.Item>
+  
+</Carousel>
+          
         <p className="text-muted mb-2 my-5">
           Usá el menú para navegar por las distintas secciones y comenzar a trabajar.
         </p>
